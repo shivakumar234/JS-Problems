9209b0c0d6eb822e77133b93401d82f5e07ac415
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:43:19 +0530

    Update reversedArray.js

--- reversedArray.js
+++ reversedArray.js
@@ -1,1 +1,21 @@
+function getReversedArray(arr) {
+    if (!Array.isArray(arr)) {
+        console.log("Input is not a valid array.");
+        return [];
+    }
 
+    const reversedArray = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArray.push(arr[i]);
+    }
+
+    return reversedArray;
+}
+
+
+const original = [10, 20, 30, 40, 50];
+const reversed = getReversedArray(original);
+
+console.log("Original Array:", original);
+console.log("Reversed Array:", reversed);
