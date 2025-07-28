ddc4130830011c97e9029cf3a746a173b0ed7300
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:41:00 +0530

    Update bookObj.js

--- bookObj.js
+++ bookObj.js
@@ -1,1 +1,22 @@
+const displayBookDetails = (bookObj)=>{
+    const {name, description, author, publishDate} = bookObj;
+    console.log(`
+        name : ${name || 'Unknown'}
+        description: ${description || 'No Description'}
+        author: ${author || 'Unknown'}
+        publishDate: ${publishDate || 'Unknown'}`);
+}
 
+const bookObj = {
+    name: "This is a Book",
+    description: "This book is about something",
+    author: 'someone',
+    publishDate: new Date().getFullYear()
+}
+const bookObj1 = {
+    name: "This is not a Book",
+    description: "This book is about You"
+}
+
+displayBookDetails(bookObj);
+displayBookDetails(bookObj1);
