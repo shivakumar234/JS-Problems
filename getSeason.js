8e566f3da14021edf2b1b6598cf1a76b8cb19b87
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:41:50 +0530

    Update getSeason.js

--- getSeason.js
+++ getSeason.js
@@ -1,1 +1,41 @@
+function getSeason(month) {
+    if (!month || typeof month !== "string") {
+        console.log("❌ Invalid month");
+        return;
+    }
 
+    switch (month.toLowerCase()) {
+        case "december":
+        case "january":
+        case "february":
+            console.log("❄️ Winter Season");
+            break;
+
+        case "march":
+        case "april":
+        case "may":
+            console.log("☀️ Summer Season");
+            break;
+
+        case "june":
+        case "july":
+        case "august":
+        case "september":
+            console.log("🌧️ Monsoon Season");
+            break;
+
+        case "october":
+        case "november":
+            console.log("🍂 Post-Monsoon / Autumn Season");
+            break;
+
+        default:
+            console.log("❌ Invalid month name");
+    }
+}
+
+
+getSeason("March");
+getSeason("August"); 
+getSeason("October");  
+getSeason("January");  
