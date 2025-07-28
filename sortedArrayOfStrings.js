75bdd5075e4dd6943671df437517352944f03091
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:43:42 +0530

    Update sortedArrayOfStrings.js

--- sortedArrayOfStrings.js
+++ sortedArrayOfStrings.js
@@ -1,1 +1,14 @@
+function sortStrings(arr) {
+    if (!Array.isArray(arr)) return [];
 
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j].toLowerCase() > arr[j + 1].toLowerCase()) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; 
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sortStrings(["Banana", "apple", "Cherry", "date"]));
