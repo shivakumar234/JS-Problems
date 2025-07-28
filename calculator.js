fe196d90b81177289a21eed266439f76077341fa
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:41:27 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
+function calculator(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+        case "x":
+            return num1 * num2;
+        case "/":
+            return num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+        case "%":
+            return num1 % num2;
+        default:
+            return "Invalid operator";
+    }
+}
 
+
+console.log(calculator(10, 5, "*"));
+console.log(calculator(10, 0, "/"));  
