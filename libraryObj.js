d7ee37110629ddefb7db527ffea95f85009c5914
shivakumar234 <shivakumarkarike234@gmail.com>
2025-07-28 08:42:14 +0530

    Update libraryObj.js

--- libraryObj.js
+++ libraryObj.js
@@ -1,1 +1,37 @@
+const library = {
+    books: [
+        {
+            title: "The Alchemist",
+            author: "Paulo Coelho",
+            year: 1988
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            year: 1960
+        }
+    ],
 
+    addBook(book) {
+        this.books.push(book);
+        console.log(`"${book.title}" has been added to the library.`);
+    },
+
+    listBooks() {
+        console.log("Library Books:");
+        this.books.forEach((book, index) => {
+            console.log(`${index + 1}. ${book.title} by ${book.author} (${book.year})`);
+        });
+    }
+};
+
+
+library.listBooks();
+
+library.addBook({
+    title: "1984",
+    author: "George Orwell",
+    year: 1949
+});
+
+library.listBooks();
